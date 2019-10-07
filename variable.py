2db04ec73156e5f2efa0e840ad4b53fce01f65b5
lion <boscro@gmail.com>
2019-10-07 14:51:21 -0400

    var

---
+++ variable.py
@@ -0,0 +1,23 @@
+greeting="stumpo"
+_myName="Jim"
+Greeting = "Hello"
+print(Greeting + ' ' + _myName)
+
+# print(Greeting + )
+
+a=12
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a // b)
+print(a % b)
+
+for i in range(1, a//b):
+    print(i)
+
+print(a + b / 3-4*12)
+print(8/2*3)
+print(8*3/2)
+print((((a+b)/3)-4)*12)
